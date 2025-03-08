28713172235404ce1b399b52941d6f870eaa7b32
Prabhu <prabu502@gmail.com>
2025-03-08 10:55:31 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+public class my {
+
+	public my() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[]args)
+	{
+		System.out.println("i am billa");
+		System.out.println("soli machi");
+	}
+
+}
